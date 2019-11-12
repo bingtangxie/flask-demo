b0e11a2f3bed6c40c99454c4c0718e8b84cc5b3c
xiebingtang <xiebingtang@163.com>
2019-11-12 20:46:56 +0800

    3 methods for adding manager command

--- manager.py
+++ manager.py
@@ -1,8 +1,32 @@
 from apps import create_app
 from flask_script import Manager
+from flask_script import Command
+
+
+class Hello(Command):
+    """hello world"""
+    def run(self):
+        print("Hello world !!!")
 
 
 app = create_app()
 manager = Manager(app)
+manager.add_command('hello', Hello())
+
+
+@manager.command
+def hello2():
+    """hello world 2 !!!"""
+    print("Hello World 2 !!!")
+
+
+@manager.option('-n', '--name', dest='name', help='Your name', default='world')
+@manager.option('-u', '--url', dest='url', default='www.csdn.com')
+def hello3(name, url):
+    """hello world or hello <setting name>"""
+    print('hello', name)
+    print(url)
+
+
 if __name__ == '__main__':
     manager.run()
